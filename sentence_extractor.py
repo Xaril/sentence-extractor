df2fe8f7a974467fb7125db38df9cdac75ae194e
Fredrik Omstedt <omstedt@kth.se>
2017-09-25 18:58:58 +0200

    Finish the sentence extractor.
    Improvements would be better sentence matching

--- sentence_extractor.py
+++ sentence_extractor.py
@@ -1,5 +1,8 @@
 import textract
 import sys
+import os
+import re
+import random
 
 ###################################
 # Extracts text from a pdf file and
@@ -9,5 +12,36 @@ import sys
 # Created by Fredrik Omstedt.
 ###################################
 
-text = textract.process(sys.argv[1])
-print(text)
+# Extracts texts from pdf files. If given a directory, the
+# program will return texts from all pdf files in that directory.
+def extractTexts():
+    file = sys.argv[1]
+    texts = []
+    if os.path.isdir(file):
+        for f in os.listdir(file):
+            if re.match(r'^.*\.pdf$', f):
+                texts.append(textract.process(file + "/" + f))
+    else:
+        texts.append(textract.process(file))
+    return texts
+
+# Chooses one sentence randomly from each of the given texts.
+def selectSentences(texts):
+    chosen_sentences = []
+    for text in texts:
+        sentence_structure = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
+        sentences = sentence_structure.findall(text)
+        chosen_sentences.append(
+            sentences[random.randint(0, len(sentences)-1)].replace("\n", " ")
+        )
+    return chosen_sentences
+
+def main():
+    texts = extractTexts()
+    sentences = selectSentences(texts)
+    for sentence in sentences:
+        print(sentence)
+        print("\n")
+
+if __name__ == '__main__':
+    main()
